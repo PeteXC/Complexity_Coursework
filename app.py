26272f4d11fa036de918535ec33bfd426233e6b1
PeteXC <peterchai2008@hotmail.co.uk>
2018-12-30 23:44:19 +0000

    Now plots population Can now plot population sitting on a 0.5 fitness landscape for visual help

--- app.py
+++ app.py
@@ -66,10 +66,13 @@ class Individual:
 ###############################################################################
 ###     Individuals Part        ###
 
-# population = [Individual() for q in range(300)]
+population = [Individual() for q in range(400)]
 
 ### Use this for printing out what the inviduals array is
 # print([Individual.G for Individual in population])
+
+###############################################################################
+#############################              ####################################
 
 ###############################################################################
 ###     SET UP FITNESS LANDSCAPE        ###
@@ -81,8 +84,8 @@ fig.suptitle("Fitness Landscape")
 #   Create the X and Y axes based on i and j values of the population
 # x0 = [Individual.i for Individual in population]
 # y0 = [Individual.j for Individual in population]
-x0 = np.arange(0,GENE_SIZE,1)
-y0 = np.arange(0,GENE_SIZE,1)
+x0 = np.arange(0, GENE_SIZE/2, 1)
+y0 = np.arange(0, GENE_SIZE/2, 1)
 X0, Y0 = np.meshgrid(x0, y0)
 
 #   Set up Z axis based on fitness values of the population
@@ -103,12 +106,15 @@ Z0 = np.multiply(z0,r0)
 ###     Plots       ###
 ###############################################################################
 
+###############################################################################
+
 ###     FITNESS       ###
 ax = fig.add_subplot(133,projection='3d')
 ax.set_aspect('equal')
 
 ax.plot_surface(X0, Y0, Z0, cmap='afmhot')
 ax.title.set_text("Fitness")
+
 
 ###############################################################################
 ###     2^i + 2^j      ###
@@ -118,7 +124,7 @@ ax1.set_aspect('equal')
 
 Z1 = np.multiply(z0,[1 for i in Y0])
 
-ax1.plot_surface(X0, Y0, Z1, cmap='afmhot')
+ax1.plot_surface(X0, Y0, Z1, cmap='afmhot', alpha=0.7)
 ax1.title.set_text("2^i + 2^j")
 
 ###############################################################################
@@ -132,5 +138,23 @@ Z2 = np.divide(Z2,z0)
 
 ax2.plot_surface(X0, Y0, Z2, cmap='afmhot')
 ax2.title.set_text("R(i,j)")
+
+###############################################################################
+###     Population Plot         ###
+
+fig2 = plt.figure()
+f2_ax = fig2.add_subplot(111,projection='3d')
+f2_ax.set_aspect('equal')
+
+popX = [Individual.i for Individual in population]
+popY = [Individual.j for Individual in population]
+popZ = [Individual.fit for Individual in population]
+
+f2_Z0 = np.multiply(z0,[0.5 for i in Y0])
+
+f2_ax.plot_surface(X0, Y0, f2_Z0, cmap='afmhot', alpha=0.5)
+f2_ax.title.set_text("Population")
+
+f2_ax.scatter(popX,popY,popZ, marker='*')
 
 plt.show()
