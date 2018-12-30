51c77dc6c2bd0011cfe21653d58f4910a12099be
PeteXC <peterchai2008@hotmail.co.uk>
2018-12-30 19:18:33 +0000

    Updated and tidied plotting
    Arranged into subplots the 3 diagrams for the fitness landscape. Commented out the population and individuals part, which will be the next step

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import random
 
 
-GENE_SIZE = 30
+GENE_SIZE = 20
 
 population = []
 
@@ -72,12 +72,13 @@ class Individual:
 # print([Individual.G for Individual in population])
 
 ###############################################################################
+###     SET UP FITNESS LANDSCAPE        ###
+
 #   Set up graphing
 fig = plt.figure()
-ax = fig.add_subplot(212,projection='3d')
-ax.set_aspect('equal')
+fig.suptitle("Fitness Landscape")
 
-# #   Create the X and Y axes based on i and j values of the population
+#   Create the X and Y axes based on i and j values of the population
 # x0 = [Individual.i for Individual in population]
 # y0 = [Individual.j for Individual in population]
 x0 = np.arange(0,GENE_SIZE,1)
@@ -86,45 +87,50 @@ X0, Y0 = np.meshgrid(x0, y0)
 
 #   Set up Z axis based on fitness values of the population
 z0 = (2**X0 + 2**Y0)
-t0 = [random.uniform(0.5,1) for i in Y0]
-# t0 = np.random.rand(len(X0),len(X0))
-# for n in range(len(X0)):
-#         for m in range(len(X0)):
-#                 t0[m,n] = (t0[m,n]+1)/2
-Z0 = np.multiply(z0,t0)
+# r0 = [random.uniform(0.5,1) for i in Y0]
+r0 = np.random.rand(len(X0),len(X0))
+for n in range(len(X0)):
+        for m in range(len(X0)):
+                r0[m,n] = (r0[m,n]+1)/2
+Z0 = np.multiply(z0,r0)
 
-print (z0)
-print ((X0))
-print ((Y0))
-print ((Z0))
-
-
-# ax.set_zlim(-1, 1000000000)
-ax.plot_surface(X0, Y0, Z0, cmap='gist_heat')
-# ax.scatter(X0,Y0,Z0)
-ax.title.set_text("Fitness Landscape")
+# print (z0)
+# print ((X0))
+# print ((Y0))
+# print ((Z0))
 
 ###############################################################################
+###     Plots       ###
+###############################################################################
 
-ax1 = fig.add_subplot(232,projection='3d')
+###     FITNESS       ###
+ax = fig.add_subplot(133,projection='3d')
+ax.set_aspect('equal')
+
+ax.plot_surface(X0, Y0, Z0, cmap='afmhot')
+ax.title.set_text("Fitness")
+
+###############################################################################
+###     2^i + 2^j      ###
+
+ax1 = fig.add_subplot(131,projection='3d')
 ax1.set_aspect('equal')
 
 Z1 = np.multiply(z0,[1 for i in Y0])
 
-ax1.plot_surface(X0, Y0, Z1, cmap='gist_heat')
+ax1.plot_surface(X0, Y0, Z1, cmap='afmhot')
+ax1.title.set_text("2^i + 2^j")
 
 ###############################################################################
+###     R(i,j)      ###
 
-ax2 = fig.add_subplot(252,projection='3d')
+ax2 = fig.add_subplot(132,projection='3d')
 ax2.set_aspect('equal')
 
-# for n in range(len(X0)):
-#         for m in range(len(X0)):
-#                 t0[m,n] = (t0[m,n]+1)/2
+Z2 = np.multiply(z0,r0)
+Z2 = np.divide(Z2,z0)
 
-Z2 = np.multiply(z0,t0)
-Z2 = np.divide(z0,t0)
-
-ax2.plot_surface(X0, Y0, Z2, cmap='gist_heat')
+ax2.plot_surface(X0, Y0, Z2, cmap='afmhot')
+ax2.title.set_text("R(i,j)")
 
 plt.show()
