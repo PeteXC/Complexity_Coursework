b896bb711d67b541c2ffa563e0e043b0c62fb555
PeteXC <peterchai2008@hotmail.co.uk>
2019-01-01 17:00:11 +0000

    Hillclimber working with graphing functionality

--- app.py
+++ app.py
@@ -107,11 +107,6 @@ done = False
 generation = 0
 
 while not(done):
-        # Fittest_list = get_Fittest(population)
-        # print(Fittest_list)
-        # for i in Fittest_list:
-        #         fittest.append(population[Fittest_list[i]])
-        # print(fittest)
 
         mutated_individual_index = random.randint(0,len(population)-1)
         # print(mutated_individual_index)
@@ -149,10 +144,9 @@ fig = plt.figure()
 fig.suptitle("Fitness Landscape")
 
 #   Create the X and Y axes based on i and j values of the population
-# x0 = [Individual.i for Individual in population]
-# y0 = [Individual.j for Individual in population]
-x0 = np.arange(0, GENE_SIZE/2, 1)
-y0 = np.arange(0, GENE_SIZE/2, 1)
+x0 = np.arange(0, (GENE_SIZE/2)+1, 1)
+y0 = np.arange(0, (GENE_SIZE/2)+1, 1)
+print(x0,y0)
 X0, Y0 = np.meshgrid(x0, y0)
 
 #   Set up Z axis based on fitness values of the population
@@ -191,7 +185,7 @@ ax1.set_aspect('equal')
 
 Z1 = np.multiply(z0,[1 for i in Y0])
 
-ax1.plot_surface(X0, Y0, Z1, cmap='copper', alpha=0.7)
+ax1.plot_surface(X0, Y0, Z1, cmap='copper')
 ax1.title.set_text("2^i + 2^j")
 
 ###############################################################################
@@ -207,28 +201,27 @@ ax2.plot_surface(X0, Y0, Z2, cmap='copper')
 ax2.title.set_text("R(i,j)")
 
 ###############################################################################
-###     Population Plot         ###
+###     Population Plot (Post)        ###
 
 fig2 = plt.figure()
-f2_ax = fig2.add_subplot(111,projection='3d')
-f2_ax.set_aspect('equal')
+f2_ax0 = fig2.add_subplot(121,projection='3d')
+f2_ax0.set_aspect('equal')
 
 popX = [Individual.i for Individual in population]
 popY = [Individual.j for Individual in population]
 popZ = [Individual.fit for Individual in population]
 
-f2_Z0 = np.multiply(z0,[0.5 for i in Y0])
+f2_Z0 = np.multiply(z0,[1 for i in Y0])
 
-f2_ax.plot_surface(X0, Y0, f2_Z0, cmap='copper', alpha=0.5)
-f2_ax.title.set_text("Population")
+f2_ax0.plot_surface(X0, Y0, f2_Z0, cmap='copper', alpha=0.7)
+f2_ax0.title.set_text("Population (Post)")
 
-f2_ax.scatter(popX,popY,popZ, marker='*')
+f2_ax0.scatter(popX,popY,popZ, marker='*')
 
 ###     Hillclimber Plot         ###
 
-fig3 = plt.figure()
-f3_ax = fig3.add_subplot(111,projection='3d')
-f3_ax.set_aspect('equal')
+f2_ax1 = fig2.add_subplot(122,projection='3d')
+f2_ax1.set_aspect('equal')
 
 hX = fittestX
 hY = fittestY
@@ -236,9 +229,9 @@ hZ = fittestZ
 
 f3_Z0 = np.multiply(z0,[1 for i in Y0])
 
-f3_ax.plot_surface(X0, Y0, f2_Z0, cmap='copper', alpha=0.5)
-f3_ax.title.set_text("Population")
+f2_ax1.plot_surface(X0, Y0, f2_Z0, cmap='copper', alpha=0.7)
+f2_ax1.title.set_text("Fittest Tracker")
 
-f3_ax.scatter(hX,hY,hZ, marker='*')
+f2_ax1.scatter(hX,hY,hZ, marker='*')
 
 plt.show()
